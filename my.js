5f7b2bb5f0f51454b081f6b0983e318f88832407
hientrinh286 <hientrinh286@gmail.com>
2021-05-18 21:10:52 +0700

    update xuong dong

--- my.js
+++ my.js
@@ -1,14 +1,14 @@
-let loan = +prompt('Nhập số tiền bạn muốn vay (Triệu Đồng):')
-let perd = +prompt("Nhập thời hạn bạn muốn vay (Tháng):")
-if (perd < 3) {
+let loan = +prompt('Nhập số tiền bạn muốn vay (Triệu Đồng):');
+let month = +prompt("Nhập thời hạn bạn muốn vay (Tháng):");
+if (month < 3) {
     rate = 0.031}
-else if (3<perd & perd<6){
+else if (3<month & month<6){
     rate = 0.05}
-else if (6<perd & perd <12) {
+else if (6<month & month <12) {
     rate = 0.69}
 else {
     rate = 0.1}
 sum = loan + loan*rate;
-document.write('Số tiền bạn vay là: '+loan);
-document.write('Thời hạn bạn vay là: '+ perd +' tháng.');
-document.write('Số tiền bạn phải trả sau ' +perd + ' tháng là:'+sum ' Triệu Đồng')
+document.write('Số tiền bạn vay là: '+loan+'<br>');
+document.write('Thời hạn bạn vay là: '+ month +' tháng.'+'<br>');
+document.write('Số tiền bạn phải trả sau ' +month + ' tháng là:'+sum +' Triệu Đồng');
